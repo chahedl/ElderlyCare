7d81063c2112f608dee418ac061e9457be71099c
Baha :) <bahaeddine.benmansour@esprit.tn>
2025-05-02 16:18:31 +0100

    Update secrets.dart

--- lib/pages/secrets.dart
+++ lib/pages/secrets.dart
@@ -1,5 +1,5 @@
-const String OPEN_API_KEY = 'AIzaSyAIytfDlXipB--L3j1oAcf51BsnUm5x-Nc';
+const String OPEN_API_KEY = '******************************';
 const String ELEVEN_LABS_API_KEY =
-    'sk_dff1665ba0226814015753465d46fcfa4170be0d5caa871b';
+    '*******************************';
 const String PublishKeyStripe =
-    'pk_test_51RHrcTGd1mOBLMSWZADSUzayt44Zg3JFdp345WK8BFIjTuki6OUj3iU5zCeKT0K8zmfWgMoGX4rBTE24EoCt2in200XMkaiB71';
+    '***************************************';
